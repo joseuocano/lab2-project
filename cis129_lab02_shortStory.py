42b5703a22690c35a01126aa15b2c64f7391d533
Jose Ocano <joseuocano@gmail.com>
2025-02-15 13:58:20 -0700

    The project: "cis129_lab02_shortStory.py" is updated by writing the actual short story that talks about my travel experience.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello World')
+# Author: Jose Ocano
+# A Young Traveler's Experience
+# Prints a simple short story
+print("What is it like to travel for the first time? For me, it is like going to a park that is different from my neighborhood." )
+print("It gives me a sense of discovery and an open mind to collect experiences from the world".)
+print("My best travel experience was when I went to New York City for my birthday.")
+print("Coming from a smaller city like Tucson, the buildings were like tall and giant monsters that covered up the skyline,")
+print("But in reality, it is just a huge city filled in with many cultures, surprises, and history.")
